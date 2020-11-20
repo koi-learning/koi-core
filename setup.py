7f0842ea711b3a496c7acc3adf8e55fe535c0d68
Johannes Richter <johannes.richter@marvin.goepel>
2020-11-20 16:31:26 +0100

    first dockerfile added;

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 import setuptools
 
 setuptools.setup(
-     name='koi-core',
-     version='0.1',
-     packages=setuptools.find_packages(),
-     scripts=['koi-worker.py'],
+    name='koi-core',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    scripts=['koi-worker.py'],
+    install_requires=[
+        "requests",
+        "configargparse",
+    ]
  )
