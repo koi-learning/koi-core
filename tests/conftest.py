39741ae9b163ae2b424e56f557d0612e5837b113
Johannes Nau <johannes.nau@tu-ilmenau.de>
2020-11-23 18:02:48 +0100

    implements basic integration test

---
+++ tests/conftest.py
@@ -0,0 +1,2 @@
+from .fixtures.api_mock import api_mock
+from .fixtures.testing_model import testing_model
---
+++ tests/fixtures/testing_model/__model__.py
@@ -0,0 +1,43 @@
+from typing import Any, List, TYPE_CHECKING
+from koi_core.resources.instance import Instance
+from koi_core.resources.sample import Sample
+
+
+def batch_generator(instance: Instance) -> List[Any]:
+    return ["batch0", "batch1"]
+
+
+def initialize_training() -> None:
+    ...
+
+
+def train(batch: List[Any]) -> None:
+    ...
+
+
+def infer(batch: List[Any], result: List[Any]) -> None:
+    ...
+
+
+def should_create_training_data() -> bool:
+    ...
+
+
+def save_training_data() -> bytes:
+    ...
+
+
+def load_training_data(data: bytes) -> None:
+    ...
+
+
+def should_create_inference_data() -> bool:
+    ...
+
+
+def save_inference_data() -> bytes:
+    ...
+
+
+def load_inference_data(data: bytes) -> None:
+    ...
