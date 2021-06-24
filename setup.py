d7228c9200bf4679c66234e8f8e44eca5d16799c
Johannes Richter <hannesrichter@users.noreply.github.com>
2021-06-24 13:18:32 +0200

    bump the version;

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='koi-core',
-    version='0.1',
+    version='0.2',
     packages=setuptools.find_packages(),
     scripts=['koi-worker.py'],
     install_requires=[
