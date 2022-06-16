8f019c7d911532730b9b8dd58ff9a22780c0a99c
Johannes Richter <hannesrichter@users.noreply.github.com>
2022-06-16 15:00:39 +0200

    updated setup.py; added workflow for tagged releases to pypi-test;

--- setup.py
+++ setup.py
@@ -9,5 +9,16 @@ setuptools.setup(
         "requests",
         "configargparse",
         "colorama",
-    ]
+    ],
+    python_requires=">=3.8",
+    url="https://github.com/koi-learning/koi-core",
+    project_urls={
+        "Github": "https://github.com/koi-learning/koi-core",
+        "Bug Tacker": "https://github.com/koi-learning/koi-core/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
  )
